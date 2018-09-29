d42839303faf64afb1f2b53d23c0a98adb782771
strawberrylin <hust.wanglin@gmail.com>
2018-09-29 21:57:33 +0800

    update the js.

--- web_front/tab_table/js/tab_table.js
+++ web_front/tab_table/js/tab_table.js
@@ -1,1 +1,19 @@
-var i = 0;
+window.onload = function (){
+  var tabs = this.document.getElementById("tabs");
+  var ul = tabs.getElementsByTagName("ul")[0];
+  var li = ul.getElementsByTagName("li");
+  var div = tabs.getElementsByTagName("div");
+
+  for(var i = 0, ln = li.length;i < ln;i ++){
+    li[i].tabIndex = i;
+    li[i].onclick = function () {
+      for(var j = 0;j < ln;j ++){
+        li[j].className = "";
+        div[j].className = 'hide';
+      }
+      this.className = "on";
+      alert(this.tabIndex);
+      div[this.tabIndex].className = "";
+    }
+  }
+}
